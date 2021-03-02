84400f5210b866cc3e164764e9c5f75c2acc5687
shokh <sh@.ru>
2021-03-02 11:42:44 +0500

    JS2

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// H/W 1
+
+let number = +prompt("Number?");
+let degree = +prompt("Degree?");
+let number2 = number;
+for (i = 1; i < degree; i++) {
+    number2 = number2 * number;
+};
+alert(number2)
+
+
+// H/W2
+

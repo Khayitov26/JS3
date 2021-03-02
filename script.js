65c7e30447da133ca305b938248c8413bf1a1f91
Your Name <you@example.co>
2021-03-02 20:21:30 +0500

    JS2

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-
 // H/W 1
 
 let number = +prompt("Number?");
 let degree = +prompt("Degree?");
 let number2 = number;
+degree = !isNaN(degree) && degree != 0 ? degree : 2;
 for (i = 1; i < degree; i++) {
-    number2 = number2 * number;
-};
-alert(number2)
-
+  number2 = number2 * number;
+}
+alert(number2);
 
 // H/W2
 

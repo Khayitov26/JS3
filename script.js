7e96c9c9f2a909120925f8b0668127cf4e6f4dbe
shokh <sh@.ru>
2021-03-03 14:53:35 +0500

    JS2

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // H/W 1
 
-// let number = +prompt("Number?");
-// let degree = +prompt("Degree?");
-// let number2 = number;
-// degree = !isNaN(degree) && degree != 0 ? degree : 2;
-// for (i = 1; i < degree; i++) {
-//   number2 = number2 * number;
-// }
-// alert(number2);
+let number = +prompt("Number?");
+let degree = +prompt("Degree?");
+let number2 = number;
+degree = !isNaN(degree) && degree != 0 ? degree : 2;
+for (i = 1; i < degree; i++) {
+  number2 = number2 * number;
+}
+alert(number2);
 
 
 // H/W2
 
-// let quantity = +prompt("Quantity?");
-// let symbol1 = prompt("Symbol1?");
-// let symbol2 = prompt("Symbol2?");
-// quantity = !isNaN(quantity) && quantity != 0 ? quantity : 5;
-// for (let i = 0; i < quantity; i++) {
-//     symbol2 = symbol1 + symbol2;
-//     console.log(symbol2);
-// }
+let quantity = +prompt("Quantity?");
+let symbol1 = prompt("Symbol1?");
+let symbol2 = prompt("Symbol2?");
+quantity = !isNaN(quantity) && quantity != 0 ? quantity : 5;
+for (let i = 0; i < quantity; i++) {
+    symbol2 = symbol1 + symbol2;
+    console.log(symbol2);
+}
 
 
 // H/W3
 
-// let name = prompt("Name?");
-// let age = +prompt("Age?");
-// let year = +prompt("Year?");
-// function old(year, age) {
-//     let years = year - age;
-//     return years
-// }
-// alert(name + ', you are ' + old(year, age) + ' years old.');  
+let name = prompt("Name?");
+let age = +prompt("Age?");
+let year = +prompt("Year?");
+function old(year, age) {
+    let years = year - age;
+    return years
+}
+alert(name + ', you are ' + old(year, age) + ' years old.');  
 
 
 // H/W4

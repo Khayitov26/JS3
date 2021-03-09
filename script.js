a8365489b4a70610c711dfb0316ee90d775a3310
shokh <sh@.ru>
2021-03-09 12:02:05 +0500

    JS3

--- script.js
+++ script.js
@@ -1,51 +1,14 @@
-// H/W 1
+//HW 1
 
-let number = +prompt("Number?");
-let degree = +prompt("Degree?");
-let number2 = number;
-degree = !isNaN(degree) && degree != 0 ? degree : 2;
-for (i = 1; i < degree; i++) {
-  number2 = number2 * number;
+let users = {}
+for (let i = 1; i < 10; i++) {
+    let username = prompt("Имя?")
+    let age = +prompt("Возраст?")
+    users.i = {
+        name: username,
+        age: age
+    }
+    console.log(users.i)
 }
-alert(number2);
+console.log (users)
 
-// H/W2
-
-let quantity = +prompt("Quantity?");
-let symbol1 = prompt("Symbol1?");
-let symbol2 = prompt("Symbol2?");
-quantity = !isNaN(quantity) && quantity != 0 ? quantity : 5;
-for (let i = 0; i < quantity; i++) {
-    symbol2 = symbol1 + symbol2;
-    console.log(symbol2);
-}
-
-// H/W3
-
-let name = prompt("Name?");
-let age = +prompt("Age?");
-let year = +prompt("Year?");
-function old(year, age) {
-    let years = year - age;
-    return years
-}
-alert(name + ', you are ' + old(year, age) + ' years old.');  
-
-
-// H/W4
-
-function maths(num1, num2) {
-    return Math.round(Math.random() * (num2 - num1) + num1)
-}
-
-let example = +prompt('Example?')
-example = !isNaN(example) && example != 0 ? example : 1;
-
-for (let i = 1; i < example; i++) {
-    let nums = maths(num1 = 1, num2 = 11)
-    let numz = maths(num1 = 1, num2 = 11)
-    let examples = +prompt(nums + ' + ' + numz + ' =')
-    let answer = (nums + numz) == examples ? 'Good job' : 'Incorrect'
-    alert(nums + ' + ' + numz + ' = ' + (nums + numz) + '.' + ' Your answer ' + examples + '. ' + answer)
-      
-}   
